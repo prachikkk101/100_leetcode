606b814a6fbaee9791567aa7dc74ea145a46131b
Prachi khandelwal <khandelwalprachi42@gmail.com>
2025-09-02 12:01:18 +0530

    Create day9.2.cpp

---
+++ day9.2.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL) return 0;
+        TreeNode*temp = root->left;
+        root->left = invertTree(root->right);
+        root->right = invertTree(temp);
+        return root;
+    }
+};
