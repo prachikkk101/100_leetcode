3cd6839cd8c7fb38c2dc6c571b674c14f5e6dd7e
Prachi khandelwal <khandelwalprachi42@gmail.com>
2025-09-07 10:19:12 +0530

    Create day14.1.cpp

---
+++ day14.1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void inorder(TreeNode* root,vector<int> &vec){
+        if(root==NULL) return;
+        inorder(root->left,vec);
+        vec.push_back(root->val);
+        inorder(root->right,vec);
+    }
+…            }
+        }
+        return min;
+        
+    }
+};
